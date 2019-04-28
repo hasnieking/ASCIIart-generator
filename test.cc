9d3441576d383012c59bd85b7dbb166615feec91
J.K. Jonkman <j.k.jonkman@umail.leidenuniv.nl>
2019-04-28 04:02:29 +0200

    added extra unit tests

--- test.cc
+++ test.cc
@@ -12,16 +12,19 @@ int main() {
     if (hextoint("A7") != 167) return 1;
     if (hextoint("C380F") != 800783) return 1;
 
-    //string dump = "424d7e0000000000000036000000280000000400000006000000010018000000000048000000c40e0000c40e00000000000000000000ffff00ffff00c0c0c0c0c0c0ffff00ffff00c0c0c0c0c0c08080ff8080ffffffffffffff8080ff8080ffffffffffffff0080ff0080ff8080808080800080ff0080ff808080808080";
+    string dump1 = "424d7e0000000000000036000000280000000400000006000000010018000000000048000000c40e0000c40e00000000000000000000ffff00ffff00c0c0c0c0c0c0ffff00ffff00c0c0c0c0c0c08080ff8080ffffffffffffff8080ff8080ffffffffffffff0080ff0080ff8080808080800080ff0080ff808080808080";
+
+    BMP *bmp = new BMP(dump1);
+    Pixel pixel = bmp->getPixel(0, 5);
+    if (pixel.red != 255 || pixel.green != 128 || pixel.blue != 0) return 1;
+    delete bmp;
 
     Hexdump *currentdump = new Hexdump("6x6bitmap.bmp");
-    string dump = currentdump->getDump();
+    string dump2 = currentdump->getDump();
 
-    BMP *bmp = new BMP(dump);
-    Pixel pixel = bmp->getPixel(5, 5);
-    //if (pixel.red != 255 || pixel.green != 128 || pixel.blue != 0) return 1;
-    cout << (int)pixel.red << ' ' << (int)pixel.green << ' ' << (int)pixel.blue << endl;
-    delete bmp;
+    bmp = new BMP(dump2);
+    pixel = bmp->getPixel(5, 5);
+    if (pixel.red != 0 || pixel.green != 0 || pixel.blue != 255) return 1;
 
     cout << "Code works correctly" << endl;
     return 0;
